fa0b6c8d6aa7e240089177581008add84fb61866
eliasc9 <ecamejo99@gmail.com>
2024-05-02 02:30:09 -0300

    Initial commit

---
+++ addons/ai/plugin.gd
@@ -0,0 +1,42 @@
+@tool
+extends EditorPlugin
+
+const MainPanel = preload("res://addons/ai/scene.tscn")
+
+var main_panel_instance
+
+var button_container: Control
+
+
+func _enter_tree():
+	main_panel_instance = MainPanel.instantiate()
+	EditorInterface.get_editor_main_screen().add_child(main_panel_instance)
+	_make_visible(false)
+	
+  # Move before AssetLib
+	var title_bar := get_tree().root.find_child("*EditorTitleBar*", true, false)
+	print(title_bar.get_children())
+	button_container = title_bar.get_child(2)
+	button_container.move_child(button_container.get_child(4), 3)
+
+
+func _exit_tree():
+	if main_panel_instance:
+		main_panel_instance.queue_free()
+
+
+func _has_main_screen():
+	return true
+
+
+func _make_visible(visible):
+	if main_panel_instance:
+		main_panel_instance.visible = visible
+
+
+func _get_plugin_name():
+	return "GodotCop"
+
+
+func _get_plugin_icon():
+	return preload("res://addons/ai/icon.svg")
---
+++ addons/ai/scene.gd
@@ -0,0 +1,36 @@
+@tool
+extends CenterContainer
+
+# BACKLOG: voice input
+var persisted = {
+	api_key_chat = "",
+	api_docs_chat = "",
+	api_key_image = "",
+	api_docs_image = "",
+	api_key_audio = "",
+	api_docs_audio = "",
+	image_style = "",
+	image_prompt = "",
+	image_generated = "",
+	chat_style = "",
+	chat_prompt = "",
+	chat_generated = "",
+	translation_style = "",
+	translation_prompt = "",
+	translation_generated = "",
+}
+
+func _load_code():
+	print(EditorInterface.get_selection().get_selected_nodes())
+	print(EditorInterface.get_script_editor().get_current_script().source_code)
+
+func _run_command():
+	var output = []
+	OS.execute("CMD.exe", ["/C", "cd %TEMP% && dir"], output)
+
+# TODO: GodotCop base actions
+#TEXTURE: style, prompt, api-key-image and api-docs
+#TODO: api-key-llm and api-docs
+#AUDIO: api-key and api-docs
+#RUN: api-key-llm and api-docs
+#TRANSLATION: api-key-llm and api-docs
